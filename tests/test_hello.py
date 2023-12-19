b5a8c2db722a9fd3bbff99a57ea6e84a8d484273
Sean Balakhanei <sean@Seans-MBP.lan>
2023-12-19 14:35:30 -0500

    Fixed unittest

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,12 @@
 import unittest
-from src.hello import say_hello
+import sys
+import os
+
+# Test from project directory in termial using: python -m unittest tests.test_hello
+# Add the 'src' folder to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+from hello import say_hello
 
 
 class TestHello(unittest.TestCase):
@@ -8,6 +15,9 @@ class TestHello(unittest.TestCase):
 
     def test_two_plus_two_equals_four(self):
         self.assertEqual(2 + 2, 4)
+    
+    def test_two_plus_two_equals_eight(self):
+        self.assertNotEqual(2 + 2, 8)
 
 
 if __name__ == "__main__":
